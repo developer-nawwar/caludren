57daf351bfd5d6ec9de0a390cdd877e474ec5c6e
developer-nawwar <nawwar.khaddaj@gmail.com>
2022-05-15 20:28:41 +0300

    add a caching service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Twig\Environment;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
@@ -28,7 +29,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -37,7 +38,8 @@ class QuestionController extends AbstractController
         ];
         $questionText="I've been turned into a cat, any thoughts on how to turn back? 
         While I'm **adorable**, I don't really care for cat food.";
-        $parsedQuestion=$markdownParser->transformMarkdown($questionText);
+        $parsedQuestion=$cache->get('markdowm_'.md5($questionText),function() use ($questionText, $markdownParser){
+        return $markdownParser->transformMarkdown($questionText);});
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
